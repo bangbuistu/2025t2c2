d07ea91a976202dedfedaffd0ea6c15cd68fde25
HoangHung <weew3426@gmail.com>
2025-11-17 10:53:02 +0700

    them file cpp

---
+++ cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"nhap vao so n";
+	cin>>n;
+	if(n%3==0){
+		cout<<"day la boi cua 3";
+	}
+	else 
+	cout<<"day khong la boi cua 3";
+}
