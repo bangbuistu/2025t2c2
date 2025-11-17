2fae60d1d07c246e4fdf5112659d03bf087a59fd
Nguyen Trong Tin <trong2004tin@gmail.com>
2025-11-17 11:21:10 +0700

    Day code cpp và sh lên

---
+++ kiemTraChanLe.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<< "Kiem tra chan le\n";
+	cout<< "Nhap so";
+	int a;
+	cin>>a;
+	if(a%2)
+	{
+		cout<<"la so le";
+	}
+	else cout<<"la so chan";
+	return 0;
+}
---
+++ kiemTraChanLe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+void main(){
+	cout<< "Kiem tra chan le\n";
+	cout<< "Nhap so";
+	int a;
+	cin>>a;
+	if(a%2)
+	{
+		cout<<"la so le";
+	}
+	else cout<<"la so chan";
+}
