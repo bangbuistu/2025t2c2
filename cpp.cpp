210fc9efa83502ee977ac92f2c2fb85a743ddf0e
NhatHuy <wywywyz123@gmail.com>
2025-11-17 10:57:59 +0700

    cpp

---
+++ cpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Nhap vao so nguyen n: ";
+	cin >> n;
+	
+	if(n<0) 
+		cout<<"la so am";
+	else
+		cout<<"la so duong";
+  return 0;
+}
