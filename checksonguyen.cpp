c444bd0dbe0fd2b01fbe4adb6e6c7da0d90ecd62
VoThiXuanThao <xuanthao943@gmail.com>
2025-11-17 10:50:27 +0700

    Chuong trinh cpp

---
+++ checksonguyen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+ int main () {
+	 int n;
+	 cout << "Nhap n";
+	 cin >> n;
+	 if (n < 0)
+	 cout << "Day la so nguyen am";
+	if (n > 0)
+	 cout << "Day la so nguyen duong";
+	 return 0;
+ }
